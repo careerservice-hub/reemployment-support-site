018c5b2ac57d4f2654c1cc08844fda5fa4fc52ca
careerservice-hub <careerdesign.ops@gmail.com>
2026-06-19 18:46:13 +0900

    Correct GitHub Pages base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://careerservice-hub.github.io',
-  base: '/reemployment-support-site',
+  base: '/reemployment-support-site/',
   integrations: [sitemap()],
 });
