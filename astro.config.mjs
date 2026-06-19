73599c9a564a4e37de0507eec8021df10885eb43
DongDouze <ha_dong_il@icloud.com>
2026-06-20 01:41:25 +0900

    Reset site for Open Design restart

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://careerservice-hub.github.io',
-  base: '/reemployment-support-site/',
-  integrations: [sitemap()],
-});
