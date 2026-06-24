ff8b2840777b5c72b4dafcbe68fb7cb0c38d39f3
careerservice-hub <careerdesign.ops@gmail.com>
2026-06-24 16:36:43 +0900

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://careerservice-hub.github.io',
-  base: '/reemployment-support-site/',
+  site: 'https://www.careerservice.co.kr',
+  base: '/',
   integrations: [sitemap()],
 });
